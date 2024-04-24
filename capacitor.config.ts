f66e1148f0607918ce01d64b0c08fdb886d01d78
Ian Losch <i.losch@45press.com>
2024-04-24 14:28:50 -0400

    refresh test android

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.feld.sxlivetiming',
-  appName: 'Supercross Live Timing',
+  appName: 'Supercross Live',
   webDir: 'dist',
   server: {
     androidScheme: 'https'
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,27 @@
-import { IonContent, IonPage } from '@ionic/react';
+import { IonContent, IonPage, IonRefresher, IonRefresherContent } from '@ionic/react';
+import React, { useState } from 'react';
 import './Home.css';
 import IframeContainer from '../components/IframeContainer';
 
 const Home: React.FC = () => {
+  const [refresh, setRefresh] = useState(0);
+
+  function handleRefresh(event: CustomEvent) {
+    setTimeout(() => {
+      // Any calls to load data go here
+      setRefresh((prevCount) => prevCount + 1); // Incrementing refresh count to trigger rerender
+      event.detail.complete();
+    }, 2000);
+  }
+
   return (
-    <IonPage className='main-wrapper'>
+    <IonPage className={'main-wrapper'}>
       <IonContent fullscreen>
+        <IonRefresher slot="fixed" onIonRefresh={handleRefresh}>
+          <IonRefresherContent></IonRefresherContent>
+        </IonRefresher>
         <IframeContainer
+          key={refresh} // Re-render the IframeContainer whenever refresh changes
           src="https://storage.googleapis.com/supercross-live-timing-leaderboard-dev/index.html"
           title="Supercross Live Timing Leaderboard"
           width="100%"
